d91ae796bf59002bfa08332339386c47ecdfc501
devaddress <harshit.sharma@swifty.global>
2021-08-04 14:20:16 +0530

    accumulated resulst added also

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ const fileName = './output/list.json';
 var file = require("./output/list.json");
 
 async function main() {
+    try{
     console.log("start script");
-    let userList = [], rewardList = [], normalRewardList = [];
+    let userList = [], rewardList = [], normalRewardList = [], accUserList = [], accRewardList = [], accNormalRewardList = [];
+    let usersA = new Map()
     const methodId = "0x0a57336a";
     let contract = await new web3.eth.Contract(abi,contractAddress);
     let stakeCount = await contract.methods.getStakingCount().call();
@@ -22,10 +24,10 @@ async function main() {
     let withdrawLimit = await contract.methods.getWithdrawLimit().call();
     console.log("stakeCount: ", stakeCount, " & rewardPercent: ", rewardPercent, "% & withdrawLimit: ", withdrawLimit, "=", withdrawLimit/(60*60*24),"days");
     const response = await axios.get(`${bscscanURL}api?module=account&action=txlist&address=${contractAddress}&startblock=1&endblock=99999999&sort=asc&apikey=YourApiKeyToken`)
-    // console.log("resposne dat: ", response.data.result[0]);
-    
+    console.log("Fetched transaction response length: ", response.data.result.length);
+    const responseLength = response.data.result.length;
     let j = 0, k = 0, l = 0;
-    for(i=0;i<response.data.result.length; i++){
+    for(i=0;i<responseLength; i++){
         if(response.data.result[i].input.slice(0,10) == methodId){
             ++j;
             const withdrawTimestamp = response.data.result[i].timeStamp;
@@ -38,6 +40,11 @@ async function main() {
                 console.log(++k, "reward this Stake ID: ", stakingId);
                 const reward = (usersTokens * rewardPercent * stakePeriod/86400)/36500;
                 console.log("user details:", stakingAddress, usersTokens, withdrawTimestamp, " - ", stakingStartTime, withdrawTimestamp-stakingStartTime, "reward: ", reward)
+                if(usersA.has(stakingAddress)) {
+                    usersA.set(stakingAddress, usersA.get(stakingAddress) + reward)
+                } else {
+                    usersA.set(stakingAddress, reward)
+                }
                 userList.push(stakingAddress);
                 rewardList.push(reward);
                 normalRewardList.push(reward/10**18);
@@ -46,10 +53,20 @@ async function main() {
         }
     }
 
+    for(var address of usersA.keys()){
+        accUserList.push(address);
+        accRewardList.push(usersA.get(address));
+        accNormalRewardList.push(usersA.get(address)/10**18);
+
+    }
+
     console.log(userList , rewardList)
     file.users = userList;
     file.rewards = rewardList;
     file.rewardsWithoutDecimals = normalRewardList;
+    file.accUserList = accUserList;
+    file.accRewardList = accRewardList;
+    file.accNormalRewardList =accNormalRewardList;
     fs.writeFileSync(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
         if (err) return console.log(err);
         console.log(JSON.stringify(file));
@@ -57,6 +74,9 @@ async function main() {
       });
 
     console.log("\n total Stakes: ", stakeCount, "\n total withdraws: ", j, "\n eligible stakes ie stake withdraws done after 30 days: ", k)
+    }catch(err){
+        console.log("error: ", err);
+    }
 }
 
 main()
